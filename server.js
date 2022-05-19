f5c927ca58b334cc94ffd3a948c8e007fe2555f0
Filip Stenström <hello@world.tld>
2022-05-19 16:01:42 +0200

    Added readme.txt and more GET logic

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const hostname = "localhost";
 const port = 8080;
 const fs = require("fs");
 const db = "flowers.json"
+const readme = "readme.txt"
 
 const server = http.createServer((req, res) => {
     const items = req.url.split("/");
@@ -11,8 +12,28 @@ const server = http.createServer((req, res) => {
     //api/rnd               \
     //api/flw/shortname     \ End goal
 
-    //GET /api/rnd
-    if (req.method === "GET" && items[2] === "rnd" && items.length === 3) {
+    // GET / || /api
+    if (req.method === "GET" && items[0] === "" || req.method === "GET" && items[0] === "" && items[1] === "api" && items.length === 2) {
+        fs.readFile(readme, "utf8", (err, data) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "text/plain");
+                res.end(data);
+        });
+    } else if (req.method === "GET" && items[1] === "api" && items[2] === "rnd" && items.length === 3) {
+        fs.readFile(db, "utf8", (err, data) => {
+            const printdata = JSON.parse(data)
+            const randomize = Math.random() * printdata.length;
+            const randomobj = Math.floor(randomize)
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(printdata[randomobj]))
+            console.log(printdata[randomobj].name); //DEBUG
+        }    
+    )};
+
+    //GET /api/rnd 
+    /*
+    if (req.method === "GET" && items[1] === "api" && items[2] === "rnd" && items.length === 3) {
         fs.readFile(db, "utf8", function(err, data) {
             const printdata = JSON.parse(data)
             const randomize = Math.random() * printdata.length;
@@ -30,7 +51,7 @@ const server = http.createServer((req, res) => {
                 console.log(printdata[randomobj].name); //DEBUG
             }
         })
-    }
+    }*/
 });
 
 server.listen(port, hostname, () => {
