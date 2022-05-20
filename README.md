4a7a3392f7e679413534a803df1b71a30764797a
Filip Stenström <hello@world.tld>
2022-05-20 10:43:10 +0200

    Completed "Write basic API (GET) in node.js (GET /name, /desc, /author)" Added readme and redirect to readme Updated README.md

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # FlowerAPI
 
-Placeholder for FlowerAPI
+FlowerAPI, API for fetching beautiful flower pics <3
 
 ## Todo
-* Write basic API (GET) in node.js (GET /name, /desc, /author)
 * Populate assets/img with beautiful flower pics <3 
+* Write API readme
 * Implement config.js (for hostname, port etc)
 * Create Dockerfile 
 
 ## Done
 * Add randomize logic in javascript (GET /rnd)
+* Write basic API (GET) in node.js (GET /name, /desc, /author)
 
 ## Nice to Have
 * Implement HTTP POST to add stuff
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require("http")
 
-const hostname = "localhost";
 const port = 8080;
 const fs = require("fs");
 const db = "flowers.json"
@@ -9,17 +8,7 @@ const readme = "readme.txt"
 const server = http.createServer((req, res) => {
     const items = req.url.split("/");
 
-    //api/rnd               \
-    //api/flw/shortname     \ End goal
-
-    // GET / || /api
-    if (req.method === "GET" && items[0] === "" || req.method === "GET" && items[0] === "" && items[1] === "api" && items.length === 2) {
-        fs.readFile(readme, "utf8", (err, data) => {
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "text/plain");
-                res.end(data);
-        });
-    } else if (req.method === "GET" && items[1] === "api" && items[2] === "rnd" && items.length === 3) {
+    if (req.method === "GET" && items[1] === "api" && items[2] === "rnd" && items.length === 3) {
         fs.readFile(db, "utf8", (err, data) => {
             const printdata = JSON.parse(data)
             const randomize = Math.random() * printdata.length;
@@ -27,33 +16,88 @@ const server = http.createServer((req, res) => {
             res.statusCode = 200;
             res.setHeader("Content-Type", "application/json");
             res.end(JSON.stringify(printdata[randomobj]))
-            console.log(printdata[randomobj].name); //DEBUG
-        }    
-    )};
+        });
+    } else if (req.method === "GET" && items[1] === "api" && items[2] === "id" && items.length === 4) {
+        fs.readFile(db, "utf8",(err, data) => {
+            const objects = JSON.parse(data);
+            const reqId = parseInt(items[3]);
+            const reqOb = objects.find((thing) => thing.id === reqId);
 
-    //GET /api/rnd 
-    /*
-    if (req.method === "GET" && items[1] === "api" && items[2] === "rnd" && items.length === 3) {
-        fs.readFile(db, "utf8", function(err, data) {
-            const printdata = JSON.parse(data)
-            const randomize = Math.random() * printdata.length;
-            const randomobj = Math.floor(randomize)
-
-            if (err) {
-                console.log(err);
-                res.statusCode = 500;
-                res.end()
-                throw err;
-            } else {
+            if (reqOb) {
                 res.statusCode = 200;
                 res.setHeader("Content-Type", "application/json");
-                res.end(JSON.stringify(printdata[randomobj]))
-                console.log(printdata[randomobj].name); //DEBUG
+                res.end(JSON.stringify(reqOb))
+            } else {
+                res.statusCode = 404
+                res.write("Not found")
+                res.end();
             }
-        })
-    }*/
+        });
+    } else if (req.method === "GET" && items[1] === "api" && items[2] === "name" && items.length === 4) {
+        fs.readFile(db, "utf8",(err, data) => {
+            const objects = JSON.parse(data);
+            const reqNm = items[3];
+            const reqOb = objects.find((thing) => thing.name === reqNm);
+
+            if (reqOb) {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify(reqOb))
+            } else {
+                res.statusCode = 404
+                res.write("Not found")
+                res.end();
+            }
+        });
+    } else if (req.method === "GET" && items[1] === "api" && items[2] === "desc" && items.length === 4) {
+        fs.readFile(db, "utf8",(err, data) => {
+            const objects = JSON.parse(data);
+            const reqDe = items[3];
+            const reqOb = objects.find((thing) => thing.description === reqDe);
+
+            if (reqOb) {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify(reqOb))
+            } else {
+                res.statusCode = 404
+                res.write("Not found")
+                res.end();
+            }
+        });
+    } else if (req.method === "GET" && items[1] === "api" && items[2] === "author" && items.length === 4) {
+        fs.readFile(db, "utf8",(err, data) => {
+            const objects = JSON.parse(data);
+            const reqAu = items[3];
+            const reqOb = objects.find((thing) => thing.author === reqAu);
+
+            if (reqOb) {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.end(JSON.stringify(reqOb))
+            } else {
+                res.statusCode = 404
+                res.write("Not found")
+                res.end();
+            }
+        });
+    } else if (req.method === "GET" && items[1] === "api" && items[2] === "readme" && items.length === 3) {
+        fs.readFile(readme, "utf8", (err, data) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/plain");
+            res.end(data);
+        });
+    } else if (req.method === "GET" && items[0] === "" || req.method === "GET" && items[0] === "" && items[1] === "api" && items.length === 2) {
+        res.writeHead(301, {"Location": "/api/readme"});
+        res.end();
+    } else if (err) {
+        console.log(err);
+        res.statusCode = 500;
+        res.end()
+        throw err; 
+    }
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running http://${hostname}:${port}`);
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 })
