0cf2d037aef985cd6fb17132fc7f89bfa136fbe9
Filip Stenström <hello@world.tld>
2022-05-31 17:35:38 +0200

    Added ID on POST return

--- server.js
+++ server.js
@@ -177,7 +177,7 @@ const server = http.createServer((req, res) => {
           console.error("Shit happens");
         }
       });
-      res.end();
+      res.end("ID: " + JSON.stringify(parseInt(getId)));
     });
   } else if (
     req.method === "PUT" &&
